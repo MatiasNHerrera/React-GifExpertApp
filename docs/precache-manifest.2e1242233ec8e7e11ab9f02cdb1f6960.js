3f113dd76a8766ff54e85c2efa9d80109e82c10c
Matias Herrera <matiassh20@gmail.com>
2020-07-10 13:01:17 -0300

    change to docs

---
+++ docs/precache-manifest.2e1242233ec8e7e11ab9f02cdb1f6960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2732b4f3cf1626aefe52208f170cbb2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b9ed7a937074f77b20",
+    "url": "/static/css/main.15681ed0.chunk.css"
+  },
+  {
+    "revision": "adf16a201702ed205573",
+    "url": "/static/js/2.200a74bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.200a74bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b9ed7a937074f77b20",
+    "url": "/static/js/main.81bab34a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
